29f9d40bbe3981b33e5b99da0c55ce414fc78836
Абдурахманов Зафаржон <zafarjan070@gmail.com>
2022-09-13 14:53:58 +0500

    JS1
    JS1

---
+++ script.js
@@ -0,0 +1,25 @@
+// 
+let str = 'aaa@bbb@ccc'
+console.log(
+    str.slice(0,3) + '!' + str.slice(4,7) + '!' + str.slice(8,11)
+)
+// 
+let hw = 'HELLOWORLD'
+hw = hw.toLowerCase(1) + hw.split(' ')
+console.log(
+    hw.charAt().toUpperCase() + hw.slice(1)
+)
+// 
+let text = 'Hello, it is HTML'
+console.log(
+    text.slice(0, 13) + 'not JS'  
+)
+// 
+let user = 'alex'
+console.log(
+    user.charAt().toUpperCase() + user.slice(1)
+)
+// 
+console.log(
+    Math.round(Math.random()*1000000)
+)
